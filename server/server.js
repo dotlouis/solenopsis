c54906dbbdfbdb20d469f9f9ed601d85b221c2e9
dot-louis <louis1.lafont@gmail.com>
2015-08-02 00:54:57 +0200

    Added node-monkey + better env management

--- server/server.js
+++ server/server.js
@@ -1,23 +1,18 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-var config = require('./config');
+var ENV = require('./env');
+
+if(ENV.NODE_ENV == 'development')
+  var nomo = require('node-monkey').start();
 
 var app = module.exports = loopback();
 
 app.start = function() {
-
-  //  Set the environment variables we need.
-  app_ipaddress = process.env.OPENSHIFT_NODEJS_IP || config.host;
-  app_port      = process.env.OPENSHIFT_NODEJS_PORT || config.port;
-
-  console.log('CONFIG');
-  console.log('Host: ', app_ipaddress);
-  console.log('Port: ', app_port);
-
   // start the web server
-  return app.listen(app_port, app_ipaddress, function() {
+  return app.listen(ENV.APP_PORT, ENV.APP_HOST, function() {
     app.emit('started');
-    // console.log('Web server listening at: %s', app.get('url'));
+    //  Set the environment variables we need.
+    console.log('Web server listening at: %s', app.get('url'));
   });
 };
 
