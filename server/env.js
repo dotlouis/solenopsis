a3166fa66df91a7d8504b6584f1648492b14b6f6
dotlouis <1891109+dotlouis@users.noreply.github.com>
2015-08-02 00:54:57 +0200

    Added node-monkey + better env management

--- server/env.js
+++ server/env.js
@@ -1,4 +1,9 @@
+var config = require('./config');
+
 module.exports = {
+  APP_HOST: process.env.OPENSHIFT_NODEJS_IP || config.host,
+  APP_PORT: process.env.OPENSHIFT_NODEJS_PORT || config.port,
+  NODE_ENV: process.env.NODE_ENV || 'production',
   FB_APP_ID: process.env.FB_APP_ID || 'Facebook Application ID',
   FB_APP_SECRET:process.env.FB_APP_SECRET || 'Facebook Application Secret'
 };
