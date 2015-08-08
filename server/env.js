c622d57e48186999dbd87f55c198b71f22523c15
dot-louis <louis1.lafont@gmail.com>
2015-08-08 16:23:12 +0200

    Setup PostgreSQL datasource

--- server/env.js
+++ server/env.js
@@ -1,9 +1,24 @@
 var config = require('./config');
 
 module.exports = {
+  // NODE
+  // 'production' by default for security reasons
+  // setting NODE_ENV to 'production' disable loopback-explorer and stacktraces in JSON responses
+  // http://docs.strongloop.com/display/public/LB/Environment-specific+configuration#Environment-specificconfiguration-Turningoffstacktraces
+  NODE_ENV: process.env.NODE_ENV || 'production',
+
+  // APP
   APP_HOST: process.env.OPENSHIFT_NODEJS_IP || config.host,
   APP_PORT: process.env.OPENSHIFT_NODEJS_PORT || config.port,
-  NODE_ENV: process.env.NODE_ENV || 'production',
-  FB_APP_ID: process.env.FB_APP_ID || 'Facebook Application ID',
-  FB_APP_SECRET:process.env.FB_APP_SECRET || 'Facebook Application Secret'
+
+  // FACEBOOK
+  FB_APP_ID: process.env.FB_APP_ID,
+  FB_APP_SECRET: process.env.FB_APP_SECRET,
+
+  // POSTGRESQL DATASOURCE
+  PG_HOST: process.env.OPENSHIFT_POSTGRESQL_DB_HOST || 'localhost',
+  PG_PORT: process.env.OPENSHIFT_POSTGRESQL_DB_PORT || 5432,
+  PG_USERNAME: process.env.OPENSHIFT_POSTGRESQL_DB_USERNAME,
+  PG_PASSWORD: process.env.OPENSHIFT_POSTGRESQL_DB_PASSWORD,
+  PG_DATABASE: process.env.PGDATABASE
 };
