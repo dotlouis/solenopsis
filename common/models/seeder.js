d325d840edad667b9f9a50e94fa3306b0429649c
dot-louis <louis1.lafont@gmail.com>
2015-10-30 16:19:15 +0100

    Added newEvent() remote Method

--- common/models/seeder.js
+++ common/models/seeder.js
@@ -1,37 +1,27 @@
 module.exports = function(Seeder) {
 
-  // // Grant admin privileges on signup (based on env var ADMIN_EMAIL)
-  // Seeder.observe('after save', function(ctx, next) {
-  //   if(ctx.instance)
-  //     if(ctx.isNewInstance)
-  //       if(ctx.instance.__data.email === process.env.ADMIN_EMAIL)
-  //         makeAdmin(ctx.instance.id);
-  //   next();
-  // });
-
-
   Seeder.grantAdmin = function(userId, cb){
     var Role = Seeder.app.models.Role;
     var RoleMapping = Seeder.app.models.RoleMapping;
 
-      // find or create the admin role
-      Role.findOrCreate({
-        name: 'admin'
-      },{
-        name: 'admin'
-      },function(err, role){
+    // find or create the admin role
+    Role.findOrCreate({
+      name: 'admin'
+    },{
+      name: 'admin'
+    },function(err, role){
+      if(err) cb(err);
+
+      // Add some users to this role
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: userId
+      }, function(err, principal) {
         if(err) cb(err);
-
-        // Add some users to this role
-        role.principals.create({
-          principalType: RoleMapping.USER,
-          principalId: userId
-        }, function(err, principal) {
-          if(err) cb(err);
-          cb();
-        });
-
+        cb();
       });
+
+    });
   };
 
   Seeder.revokeAdmin = function(userId, cb){
@@ -42,17 +32,43 @@ module.exports = function(Seeder) {
       name: 'admin'
     }}, function(err, role){
       if(err) cb(err);
-      if(!role) cb("Admin role does not exist");
+      if(!role) cb('Admin role does not exist');
 
       RoleMapping.destroyAll({
         roleId: role.id,
         principalId: userId
       }, function(err, info){
         if(err) cb(err);
-        if(info.count === 0) cb("User not mapped to admin role");
-        cb()
+        if(info.count === 0) cb('User not mapped to admin role');
+        cb();
       });
     });
   };
 
+  Seeder.prototype.newEvent = function(eventData, cb){
+    var Event = Seeder.app.models.Event;
+    var self = this;
+
+    self.events.create(eventData, function(err, event){
+      if(err) cb(err);
+      self.following.add(event, function(err2){
+        if(err2) cb(err2);
+        cb(null, event);
+      });
+    });
+
+  };
+
+  Seeder.remoteMethod('newEvent',{
+    http: {verb:'post'},
+    isStatic: false,
+    description: 'Creates an event for the user and automatically follows it',
+    accepts: {
+      arg: 'event',
+      type: 'object',
+      required: true,
+      http:{source:'body'}
+    },
+    returns: {arg: 'event', type: 'object'}
+  });
 };
